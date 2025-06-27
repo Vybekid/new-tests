ab930d083f720c3ce9b24efbb5910cf8fc773c53
Vybekid <felobitto@gmail.com>
2025-06-27 23:07:42 +0300

    project

--- stepbystep.py
+++ stepbystep.py
@@ -1,13 +1,23 @@
+#import the module to design graphs and draw stuff
 import turtle
 
+#Set up the screen
 screen = turtle.Screen()
+#set up the screen color
 screen.bgcolor("yellow")
+#initialize turtle object, such as forward as T
 t = turtle.Turtle()
+#initialize speed(0-10) being slow
 t.speed(3)
+#hide the drawing cursor when not in use
 t.hideturtle()
 
+t.pencolor("white")
+t.pensize(3)
 
 
 
 
+
+#keep the screen active when the drawing is done
 turtle.done()
